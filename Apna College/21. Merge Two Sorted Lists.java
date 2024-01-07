f5bbf2c45f772756df6e1f05aba0542a2438af55
Ullas B C <56510307+ullasbc-02@users.noreply.github.com>
2024-01-07 07:45:22 +0530

    Adding explanation

--- Apna College/21. Merge Two Sorted Lists.java
+++ Apna College/21. Merge Two Sorted Lists.java
@@ -8,6 +8,28 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// EXPLANATION
+
+// Maintain a head and a tail pointer on the merged linked list.
+
+// Then choose the head of the merged linked list by comparing the first node of both linked lists.
+
+// For all subsequent nodes in both lists, you choose the smaller current node and link it to the tail of the merged list, and moving the current pointer of that list one step forward.
+
+// You keep doing this while there are some remaining elements in both the lists.
+
+// If there are still some elements in only one of the lists, you link this remaining list to the tail of the merged list.
+
+// Initially, the merged linked list is NULL.
+
+// Compare the value of the first two nodes and make the node with the smaller value the head node of the merged linked list.
+
+// Since it’s the first and only node in the merged list, it will also be the tail.
+
+// Then move head1 one step forward.
+
+    
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode head = new ListNode(0);
@@ -29,8 +51,5 @@ class Solution {
             handler.next=list2;
         }
         return head.next;
-        
-
-        
     }
 }
