67034fd8db22ea3686bb989f4d99a4e2fb94aa39
ullasbc02 <ullasbc02@gmail.com>
2024-02-13 06:38:02 +0530

    Moving QuickSort

---
+++ Sorting/QuickSort.java
@@ -0,0 +1,42 @@
+package Sorting;
+
+//https://www.youtube.com/watch?v=UA_Rmjfj4bw&t=1328s
+
+public class QuickSort {
+    public static void Swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static int Partition(int arr[], int left, int right){
+        int pivot = arr[left];
+        int i = left;
+        int j = right;
+        while(i<j){
+            while(arr[i]<=pivot && i<=right-1) i++;
+            while(arr[j]>pivot && j>=left) j--;
+            if(i<j){
+                Swap(arr, i, j);
+            }
+        }
+        Swap(arr,j,left);
+        return j;
+    }
+
+    public static void Quicksort(int arr[], int left, int right){
+        
+        if(left<right){
+            int pivot = Partition(arr, left, right);
+            Quicksort(arr, left, pivot-1);
+            Quicksort(arr, pivot+1, right);
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {10, 7, 8, 9, 1, 5};
+        Quicksort(arr, 0, arr.length - 1);
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
